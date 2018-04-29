71c74d54709eb490e67a548ab261ada8dade369d
Ben <ben.feather@xtra.co.nz>
2018-04-29 15:40:56 +1200

    Gulpfile PostCss

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function () {
+    console.log('Gulp Task')
+})
+
+gulp.task('html', function () {
+    console.log('do something to html')
+})
+
+gulp.task('styles', function () {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+})
+
+gulp.task('watch', function () {
+
+    watch('./app/index.html', function () {
+        gulp.start('html')
+    })
+
+    // Watch all .css files (including those in subfolders)
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('styles')
+    })
+})
